bad7e9ea2c4d18e116e74876ad879924192105e0
Brice VIGNAL <vignal.brice@gmail.com>
2022-02-23 19:50:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+import "./assets/styles/main.css";
+
+const adviceId = document.querySelector(".advice-id");
+const advice = document.querySelector(".advice");
+
+const fetchAdvices = async () => {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    if (response.ok) {
+      const data = await response.json();
+      adviceId.innerHTML = `Advice #${data.slip.id}`;
+      advice.innerHTML = `<span>&#8220;</span>${data.slip.advice}<span>&#8221;</span>`;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+fetchAdvices();
+setInterval(() => fetchAdvices(), 4000);
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetchAdvices();
+});
